51ecb250ae7d026b53d744d929180bf65f127351
Christopher Bowns <github@cbowns.com>
2014-01-27 17:09:50 -0800

    Remove pause on launch test code

--- RACTest/MPSTicker.m
+++ RACTest/MPSTicker.m
@@ -49,13 +49,6 @@ static NSUInteger const kAccumulationDisabled = 0;
 
 		// Accumulate by default.
 		_accumulateEnabled = YES;
-
-		// For testing…
-		double delayInSeconds = 2.0;
-		dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-		dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-			self.accumulateEnabled = NO;
-		});
 	}
     return self;
 }
